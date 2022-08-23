e1172b63bed0c28bf33f9719440a3a760bcc9c3a
Nilufa Khan <khannilufa5@gmail.com>
2022-08-23 20:10:52 +0530

    Sprint1

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -2,21 +2,36 @@ package com.jap;
 
 public class AverageAge {
 
-    public String averageAgeCalculator(String[] ageRawData)
-    {
-        double avg = 0;
-
+    public String averageAgeCalculator(String[] ageRawData){
+        int[] resultantArray = new int[ageRawData.length];
+        String output = "";
         try{
-            //Write the logic
+            for(int i = 0 ; i < ageRawData.length ;i++){
+                resultantArray[i] = Integer.parseInt(ageRawData[i]);
 
-            //Handle specific exception
-        }catch (Exception exception)
-        {
-            return exception.toString();
+            }
+            double sum = 0;
+            for (int i = 0; i < resultantArray.length; i++) {
+                sum = sum + resultantArray[i];
+            }
+            output = Double.toString(sum / resultantArray.length);
+
+
+
+        }catch(NumberFormatException e){
+            output = e.toString();
         }
-        return String.valueOf(avg);
+        return output;
     }
 
-
-
+    public static void main(String[] args) {
+        AverageAge obj = new AverageAge();
+        AverageAge obj2 = new AverageAge();
+        String[] age = {"35", "26", "32", "44"};
+        String[] age2 = {"35","26","thirty-two","44"};
+        String output = obj.averageAgeCalculator(age);
+        System.out.println("output = " + output);
+        String output2 = obj.averageAgeCalculator(age2);
+        System.out.println("output2 = " + output2);
+    }
 }
